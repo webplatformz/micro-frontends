9d9a51810b411e9f1c7f5387f7e1e87d676bb967
r3dDoX <patrick@walther-online.ch>
2018-06-05 15:48:34 +0200

    reformat code style

--- frontend-react/react-app.js
+++ frontend-react/react-app.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import App from './src/App';
 
 window.customElements.define('react-app', class extends HTMLElement {
 
-    constructor() {
-        super();
-        console.log('ReactApp constructor', this);
-    }
+  constructor() {
+    super();
+    console.log('ReactApp constructor', this);
+  }
 
-    connectedCallback() {
-        console.log('ReactApp connected');
-        this.render();
-    }
+  connectedCallback() {
+    console.log('ReactApp connected');
+    this.render();
+  }
 
-    render() {
-        render(<App/>);
-    }
+  render() {
+    render(<App />);
+  }
 
 });
