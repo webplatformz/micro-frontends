a0ef4779a981114fd361d19cd7297a60b8e4d1dc
r3dDoX <patrick@walther-online.ch>
2018-06-05 13:10:51 +0200

    Revert "introduce react-app-rewired to extend babel config"
    This reverts commit f02c44b

--- frontend-react/config-overrides.js
+++
@@ -1,7 +0,0 @@
-const {injectBabelPlugin} = require('react-app-rewired');
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin('transform-custom-element-classes',config);
-  config = injectBabelPlugin('transform-es2015-classes',config);
-  return config;
-};
