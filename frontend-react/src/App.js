f1ebea9021295de4ed220d328aa2b18d5942504b
Nick Payne <nick.payne89@gmail.com>
2018-06-06 11:49:04 +0200

    push it to the limit

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,19 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    textEntered(element) {
+        const text = element.value;
+        const event = new CustomEvent('textEntered', {
+            bubbles: true,
+            detail: {
+                text
+            }
+        });
+
+        element.dispatchEvent(event);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <input type="text" placeholder="tue da öpis dri" onKeyUp={event => this.textEntered(event.target)}/>
+            </div>
+        );
+    }
 }
 
 export default App;
